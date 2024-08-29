ec512504b882782d9db0404f9f0dc725bf0f6c2e
robingithb <robinthomasq007@gmail.com>
2024-08-29 16:57:04 +0530

    bottom sheet content

--- lib/ui/homescreen.dart
+++ lib/ui/homescreen.dart
@@ -8,6 +8,70 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
+  List<Map<String, dynamic>> _allNotes = [];
+  bool _isLoading = true;
+
+  final TextEditingController _noteTitleContorller = TextEditingController();
+  final TextEditingController _noteDescriptionController =
+      TextEditingController();
+
+  void showBottomsheetContent(int? id) async {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (_) => SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                        top: 15,
+                        left: 15,
+                        right: 15,
+                        bottom: MediaQuery.of(context).viewInsets.bottom),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          TextField(
+                            controller: _noteTitleContorller,
+                            decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Note Title"),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          TextField(
+                            controller: _noteDescriptionController,
+                            maxLines: 5,
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Description",
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Center(
+                            child: OutlinedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Add Note",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w300),
+                                )),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +79,14 @@ class _HomescreenState extends State<Homescreen> {
         title: const Text("Note"),
         centerTitle: true,
       ),
-      body: Center(
+      body: const Center(
         child: Text("notes"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showBottomsheetContent(null);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
